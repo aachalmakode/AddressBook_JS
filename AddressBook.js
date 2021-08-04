28c9ea9fab74888205ce8c7b9f3ff988bc400e67
Aachal <aachalmakode900@gmail.com>
2021-08-04 19:43:43 -0400

    Added: Creating asddressbook and adding one contact

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,30 @@
 console.log("Welcome to address book system")
+class Contact {
+
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    toString() {
+        return "First name: " + this.firstName + "\nLast name: " + this.lastName + "\nAddress: " + this.address + "\nCity: " + this.city + "\nState: " + this.state + "\nZip: " + this.zip + "\nPhone number: " + this.phoneNumber + "\nEmail: " + this.email;
+    }
+}
+
+let contact = new Contact("Aachal", "Makode", "Nirmala colony", "Amravati", "Maharashtra", "444602", "7777744499", "aachalmako@gmail.com");
+console.log(contact.toString());
